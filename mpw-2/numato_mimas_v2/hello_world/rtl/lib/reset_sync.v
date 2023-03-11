de1d3b2443c0be72405fa2808f5537a1a8859ee2
dineshannayya <dinesh.annayya@gmail.com>
2023-03-11 17:52:13 +0530

    Reset sync update

--- mpw-2/numato_mimas_v2/hello_world/rtl/lib/reset_sync.v
+++ mpw-2/numato_mimas_v2/hello_world/rtl/lib/reset_sync.v
@@ -8,11 +8,14 @@ assign reset_n = reset_d3;
 
 reg reset_d1,reset_d2,reset_d3;
 
+reg [7:0] cnt;
+
 initial
 begin
    reset_d1 = 0;
    reset_d2 = 0;
    reset_d3 = 0;
+	cnt      = 0;
 end
 
 
@@ -20,7 +23,10 @@ always @(posedge mclk)
 begin
    reset_d1 <= 1'b1;
    reset_d2 <= reset_d1;
-   reset_d3 <= reset_d2;
+	if(cnt < 8'hFF) cnt <= cnt + 1;
+	else begin
+      reset_d3 <= reset_d2;
+   end
 end
    
 
