bb676507bcfd2d9a2bb55a18765c944f38343b10
dineshannayya <dinesh.annayya@gmail.com>
2023-06-20 22:45:18 +0530

    2206Q test case

---
+++ 2206Q/tests/wb_port/wb_port.c
@@ -0,0 +1,186 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This include is relative to $CARAVEL_PATH (see Makefile)
+#include <defs.h>
+#include <stub.h>
+#include "ext_reg_map.h"
+
+
+
+
+
+
+/*
+	Wishbone Test:
+		- Configures MPRJ lower 8-IO pins as outputs
+		- Checks counter value through the wishbone port
+*/
+int i = 0; 
+int clk = 0;
+
+void delay(const int d)
+{
+
+    /* Configure timer for a single-shot countdown */
+	reg_timer0_config = 0;
+	reg_timer0_data = d;
+    reg_timer0_config = 1;
+
+    // Loop, waiting for value to reach zero
+   reg_timer0_update = 1;  // latch current value
+   while (reg_timer0_value > 0) {
+           reg_timer0_update = 1;
+   }
+
+}
+
+void putdword(uint32_t Data)
+{
+	reg_uart_data = Data >> 24; // MSB [31:24];
+	reg_uart_data = Data >> 16; // MSB [23:16];
+	reg_uart_data = Data >> 8;  // MSB [15:8];
+	reg_uart_data = Data;       // MSB [7:0];
+	delay(800000);
+}
+
+
+void configure_io()
+{
+
+//  ======= Useful GPIO mode values =============
+
+//      GPIO_MODE_MGMT_STD_INPUT_NOPULL
+//      GPIO_MODE_MGMT_STD_INPUT_PULLDOWN
+//      GPIO_MODE_MGMT_STD_INPUT_PULLUP
+//      GPIO_MODE_MGMT_STD_OUTPUT
+//      GPIO_MODE_MGMT_STD_BIDIRECTIONAL
+//      GPIO_MODE_MGMT_STD_ANALOG
+
+//      GPIO_MODE_USER_STD_INPUT_NOPULL
+//      GPIO_MODE_USER_STD_INPUT_PULLDOWN
+//      GPIO_MODE_USER_STD_INPUT_PULLUP
+//      GPIO_MODE_USER_STD_OUTPUT
+//      GPIO_MODE_USER_STD_BIDIRECTIONAL
+//      GPIO_MODE_USER_STD_ANALOG
+
+
+//  ======= set each IO to the desired configuration =============
+
+    //  GPIO 0 is turned off to prevent toggling the debug pin; For debug, make this an output and
+    //  drive it externally to ground.
+
+    reg_mprj_io_0 = GPIO_MODE_MGMT_STD_ANALOG;
+
+    // Changing configuration for IO[1-4] will interfere with programming flash. if you change them,
+    // You may need to hold reset while powering up the board and initiating flash to keep the process
+    // configuring these IO from their default values.
+
+    reg_mprj_io_1 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_2 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+    reg_mprj_io_3 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+    reg_mprj_io_4 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+
+    // -------------------------------------------
+
+    reg_mprj_io_5 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;     // UART Rx
+    reg_mprj_io_6 = GPIO_MODE_MGMT_STD_OUTPUT;           // UART Tx
+    reg_mprj_io_7 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_8 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_9 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_10 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_11 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_12 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_13 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_14 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_15 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+    reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_34 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_35 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+    // Initiate the serial transfer to configure IO
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+}
+
+
+void main()
+{
+
+	int bFail = 0;
+    reg_gpio_mode1 = 1;
+    reg_gpio_mode0 = 0;
+    reg_gpio_ien = 1;
+    reg_gpio_oe = 1;
+
+
+    configure_io();
+    reg_uart_enable = 1;
+    reg_wb_enable = 1;
+
+
+    reg_la0_oenb = reg_la0_iena = 0xFFFFFFFF;    // [31:0]
+    reg_la0_data = 0x000;
+    reg_la0_data = 0x001; // Remove Soft Reset
+
+
+    // Remove Wishbone Reset
+    reg_mprj_wbhost_ctrl = 0x1;
+
+    // Remove Reset
+    reg_glbl_cfg0 = 0x01f;
+
+    putdword(reg_glbl_chip_id);
+    putdword(reg_glbl_soft_reg_0);
+    putdword(reg_glbl_soft_reg_1);
+    putdword(reg_glbl_soft_reg_2);
+
+    
+    // blink the led if all checks passes
+    while (bFail == 0) {
+        if (reg_glbl_chip_id != 0x82682501)    bFail = 1; // check chip id
+        if (reg_glbl_soft_reg_0 != 0x82738343) bFail = 1; // check signature
+        if (reg_glbl_soft_reg_1 != 0x07092022) bFail = 1; // check date
+        if (reg_glbl_soft_reg_2 != 0x00054000) bFail = 1; // check version
+        reg_gpio_out = 1; // OFF
+		delay(8000000);
+        reg_gpio_out = 0;  // ON
+		delay(8000000);
+
+    }
+
+}
