a3c367c1d05f226065c63834a6bd7781363eee32
Dinesh Annayya <dinesh.annayya@gmail.com>
2023-07-30 16:52:15 +0530

    line follower

---
+++ 2206Q/tests/btest_line_follower/btest_line_follower.ino
@@ -0,0 +1,35 @@
+/*************************************************************
+  Connection Setting of RoboMart Line Follower Development Kit
+  Reference: https://www.scribd.com/document/238827643/User-Manual-IBOT-Mini-V3#
+
+LEDs  ---@----------------> PB1  - D9   -->  Output
+LEDs  ---@----------------> PB2  - D10  -->  Output
+LEDs  ---@----------------> PB3  - D11  -->  Output
+LEDs  ---@----------------> PB4  - D12  -->  Output
+Left Sensor  -------------> PC3 -  A3   -->  Input
+Right Sensor -------------> PC0 -  A0   -->  Input
+Buzzer       -------------> PD4 -  D4   ---> Output
+Right Motor (+) ----------> PB1  - D9   ---> Output
+Right Motor (+) ----------> PB2  - D10  ---> Output
+Right Motor (+) ----------> PB3  - D11  ---> Output
+Right Motor (+) ----------> PB4  - D12  ---> Output
+Buzzer          ----------> PD4  - D4   ---> Output
+RESET --------------------> PC6  - RESET
+Crystall Oscialltor (12Mhz) --> PB6 and PB7
+VB = Battery Supply
+VCC = Regulated 5V+
+Gnd = Gnd (0V)
+
+
+
+*************************************************************/
+
+  void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
